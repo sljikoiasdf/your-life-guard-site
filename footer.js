b5b917150c0d93ea9a59205f7766e6f9a7ad788c
sljikoiasdf <stephen@saveu.com.au>
2026-08-04 13:03:54 +1000

    Update Financial Services Guide link in footerUpdate Financial Services Guide link to YLG_FSG_20260730.pdf

--- footer.js
+++ footer.js
@@ -32,7 +32,7 @@
         '<div class="footer-col">' +
           '<h4>Legal</h4>' +
           '<a href="privacy-policy.html">Privacy Policy</a>' +
-          '<a href="https://saveucomau.sharepoint.com/:b:/s/LifeExperts/IQAUs5DjWaDaS4wtyEu81DFpAeVQ5xB0ianDspOv0teaLDg?e=2vvRMz" target="_blank" rel="noopener">Financial Services Guide</a>' +
+          '<a href="https://saveucomau.sharepoint.com/:b:/s/LifeExperts/IQD70VuU0O6NSLor3HQcpdRFAeLxZq44HCLXOCWseoEFB0s?e=hrTw8a" target="_blank" rel="noopener">Financial Services Guide</a>' +
         '</div>' +
       '</div>' +
 
@@ -41,7 +41,7 @@
         '<p>Your Life Guard only provides general advice, which means we haven’t considered your individual financial situation, objectives or needs. Before acting on it, please consider the appropriateness of the advice, having regard to these factors.</p>' +
         '<p>Before making a decision to purchase or continue to hold a life insurance product, you should read the relevant Product Disclosure Statement (PDS). The PDS contains the terms and conditions of the product and the details of the product issuer, and you should consider it prior to purchase. The Target Market Determination for each product is also available. PDSs for the financial products on this website are available by calling us on 1300 718 859 or by visiting the website of each product provider.</p>' +
         '<p>Whilst Your Life Guard attempts to provide comparisons from as many providers as possible, not all providers of insurance products are included in our comparisons. Prior to making any financial decisions regarding financial products, we highly recommend that you seek individual taxation and legal advice relevant to your particular situation.</p>' +
-        '<p>Please view the <a href="https://saveucomau.sharepoint.com/:b:/s/LifeExperts/IQAUs5DjWaDaS4wtyEu81DFpAeVQ5xB0ianDspOv0teaLDg?e=2vvRMz" target="_blank" rel="noopener">Financial Services Guide here</a> and our <a href="privacy-policy.html">Privacy Policy here</a>.</p>' +
+        '<p>Please view the <a href="https://saveucomau.sharepoint.com/:b:/s/LifeExperts/IQD70VuU0O6NSLor3HQcpdRFAeLxZq44HCLXOCWseoEFB0s?e=hrTw8a" target="_blank" rel="noopener">Financial Services Guide here</a> and our <a href="privacy-policy.html">Privacy Policy here</a>.</p>' +
       '</div>' +
     '</div>' +
   '</footer>';
